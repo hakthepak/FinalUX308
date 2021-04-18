ee989887f184cc68f28b3ae929765f14b1c719f2
hakthepak <hakx7820@mylaurier.ca>
2021-04-18 18:43:15 -0400

    final commit

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"Home", sName:"Richard Hildred"});
+        super({title:"Home", sName:"Samer Hak"});
     }
     render(sPage) {
-        const oJson = fetch("https://prog8110winter2021section2-default-rtdb.firebaseio.com/meals.json").json();
+        const oJson = fetch("https://ux308final-default-rtdb.firebaseio.com/meals.json").json();
         console.log(oJson);
-        let sResult = "<h1>Upcoming Popup Meals</h1>";
+        let sResult = "<h1>DIY CRAFTS</h1>";
         Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
@@ -17,7 +17,9 @@ module.exports = class extends Page {
             <h2>${oEntity.title}</h2>
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
             <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
+            <p>${oEntity.Review}</p>
+            <p>${oEntity.Related}</p>
+            <form action="https://payment-assignment-3.herokuapp.com/payment" method="post">
             <input type="hidden" name="title" value="${oEntity.title}" />
             <input type="hidden" name="price" value="21" />
             <input type="tel" placeholder="enter your number" name="telephone"/>
